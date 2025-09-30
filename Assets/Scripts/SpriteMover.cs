390b924da38deddfd777e0e6fd292317decd4cd0
KeeganKerr <fireyone839@gmail.com>
2025-09-30 11:38:01 -0700

    Sprite mover project
    contains the files for the sprite mover

---
+++ Assets/Scripts/SpriteMover.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour
+{
+    // sets the min and max distance the sprite can teleport
+    public float xRange = 5f;
+    public float yRange = 5f;
+
+    void Update()
+    {
+        // Teleports when key is pressed "space"
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetNewPosition();
+        }
+    }
+
+    void SetNewPosition()
+    {
+       
+          // Determines the area the sprite can teleport to
+        float randomX = Random.Range(-xRange, xRange);
+        float randomY = Random.Range(-yRange, yRange);
+        Vector2 newPosition = new Vector2(randomX, randomY);
+        
+
+
+        //Updates position of the sprite
+        transform.position = newPosition;
+    }
+}
